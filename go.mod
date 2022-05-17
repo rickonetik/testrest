04ac871a0f9eff7d7022487f60fa111eaebf3247
rickonetik <rickonetik@gmail.com>
2022-05-17 14:03:19 +0300

    api

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rickonetik/reg_api
+
+go 1.18
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+type Found struct {
+	Id       int    `json: "id"`
+	Login    string `json: "login"`
+	Password string `json: "password"`
+	Carma    int    `json: "carma"`
+}
+
+func main() {
+}
